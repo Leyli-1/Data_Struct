a71ceb7ca49efbf39a22ae7ea9a28b953a4e7726
Leyli-1 <abgaryanleyli99@gmail.com>
2024-12-20 22:01:32 +0400

    Exam test
    2nd midterm exam YSU

---
+++ Toms_10.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <queue>
+#include <climits>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+void printLevelMax(TreeNode* root) {
+    if (!root) return;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = q.size();
+        int maxVal = INT_MIN;
+
+        for (int i = 0; i < levelSize; ++i) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            maxVal = max(maxVal, node->val);
+
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+
+        cout << "Max at this level: " << maxVal << endl;
+    }
+}
+
+TreeNode* buildTree() {
+    cout << "Enter the value of root node (-1 for no node): ";
+    int val;
+    cin >> val;
+
+    if (val == -1) return nullptr;
+
+    TreeNode* root = new TreeNode(val);
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        cout << "Enter the left child of " << current->val << " (-1 for no node): ";
+        cin >> val;
+        if (val != -1) {
+            current->left = new TreeNode(val);
+            q.push(current->left);
+        }
+
+        cout << "Enter the right child of " << current->val << " (-1 for no node): ";
+        cin >> val;
+        if (val != -1) {
+            current->right = new TreeNode(val);
+            q.push(current->right);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode* root = buildTree();
+    printLevelMax(root);
+    return 0;
+}
---
+++ Ռեկուրսիվ_toms_10.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+void findLevelMax(TreeNode* root, vector<int>& maxValues, int level) {
+    if (!root) return;
+
+    if (level == maxValues.size()) {
+        maxValues.push_back(root->val);
+    }
+    else {
+        maxValues[level] = max(maxValues[level], root->val);
+    }
+
+    findLevelMax(root->left, maxValues, level + 1);
+    findLevelMax(root->right, maxValues, level + 1); 
+}
+
+void printLevelMax(TreeNode* root) {
+    vector<int> maxValues;
+    findLevelMax(root, maxValues, 0);
+
+    for (int i = 0; i < maxValues.size(); ++i) {
+        cout << "Max arjeq: " << maxValues[i] << endl;
+    }
+}
+
+TreeNode* buildTree() {
+    cout << "armat: ";
+    int val;
+    cin >> val;
+
+    if (val == -1) return nullptr; 
+
+    TreeNode* root = new TreeNode(val);
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        cout << "Mutq " << current->val << " dzaxi arjeqy: ";
+        cin >> val;
+        if (val != -1) {
+            current->left = new TreeNode(val);
+            q.push(current->left);
+        }
+
+        cout << "Mutq " << current->val << " aji mutqy: ";
+        cin >> val;
+        if (val != -1) {
+            current->right = new TreeNode(val);
+            q.push(current->right);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode* root = buildTree();
+
+    printLevelMax(root);
+
+    return 0;
+}
